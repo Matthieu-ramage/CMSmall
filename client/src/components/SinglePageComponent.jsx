899377d7116e9b9875ce1f865dbf487db2f4a620
Ramage <ramagematthieu@gmail.com>
2023-06-24 14:03:38 +0200

    ajout update page delete blokc

--- client/src/components/SinglePageComponent.jsx
+++ client/src/components/SinglePageComponent.jsx
@@ -1,31 +1,16 @@
 import { Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import API from '../API';
 import Blocks from './BlockComponent';
 
 function SinglePage(props) {
-  // get the questionId from the URL to retrieve the right question and its answers
   const params = useParams();
   const page = props.pages[params.pageId-1];
-  const [blocks, setBlocks] = useState([]);
-  
-  const getBlocks = async () => {
-    const blocks = await API.getBlock(params.pageId);
-    setBlocks(blocks);
-  }
-
-  useEffect(()=> {
-    // get all the blocks of this page from API
-    getBlocks();
-  }, []);
   
   return (
     <>
-    {/* The check on "question" is needed to intercept errors due to invalid URLs (e.g., /questions/5 when you have two questions only) */}
     {page ? <>
       <PageDescription page={page} />
-      <Blocks blocks={blocks} loggedIn={props.loggedIn}></Blocks></> :
+      <Blocks pageId={params.pageId} loggedIn={props.loggedIn}></Blocks></> :
       <p className='lead'>The selected page does not exist!</p>
     } 
     </>
